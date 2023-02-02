d9a4e31a92220a7647f77bfda64a051b41545605
Анастасия Миронова <nastymironkiss@gmail.com>
2023-02-02 21:21:22 +0700

    create model category and api and review apps

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.core import validators
+
+
+class Category(models.Model):
+    """Класс категорий"""
+    name = models.CharField('Название', max_length=256)
+    slug = models.SlugField(
+        'Cлаг',
+        max_length=50,
+        unique=True,
+        validators=[validators.RegexValidator(
+            regex=r'^[-a-zA-Z0-9_]+$',
+            message='Слаг содержит недопустимые символы'
+        )]
+    )
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+    
+    def __str__(self) -> str:
+        return self.name
