31c2f4e5115b177480fcb88d12f3ff742594ab45
Анастасия Миронова <nastymironkiss@gmail.com>
2023-02-04 18:37:48 +0700

    change urls.py in api app and in api_yamdb folder

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 
 from .views import CategoryViewSet
 
-router = routers.DefaultRouter()
-router.register('categories', CategoryViewSet, basename='categories')
+v1_router = routers.DefaultRouter()
+v1_router.register('categories', CategoryViewSet, basename='categories')
 urlpatterns = [
-    path('', include(router.urls))
+    path('v1/', include(v1_router.urls))
 ]
